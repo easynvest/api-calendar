b06eeca603b070d2b013ad6429dd069659743650
Edmo Lima <edmo.lima@easynvest.com.br>
2017-09-25 15:28:43 -0300

    Adiciona redis a app.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+// require the dependencies we installed
+var app = require('express')();
+var responseTime = require('response-time')
+var axios = require('axios');
+var redis = require('redis');
+
+// create a new redis client and connect to our local redis instance
+var client = redis.createClient();
+
+// if an error occurs, print it to the console
+client.on('error', function (err) {
+    console.log("Error " + err);
+});
+
+app.set('port', (process.env.PORT || 3000));
 
 app.use('/foo', require('./controllers/foo'))
 
-app.listen(3000)
+app.listen(app.get('port'), function(){
+  console.log('Server listening on port: ', app.get('port'));
+});
