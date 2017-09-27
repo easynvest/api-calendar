ed6133290539e8230cf29d40eb087d33326506da
Edmo Lima <edmo.lima@easynvest.com.br>
2017-09-27 17:48:44 -0300

    Calculate number weekends.

--- src/modules/manager_calendar.js
+++ src/modules/manager_calendar.js
@@ -1,19 +1,30 @@
 const moment = require('moment')
-moment.locale('pt-BR')
 
 const manager_calendar = {
   calendarData: (req) => {
     const data = {
-      businessDays: 200,
+      businessDays: manager_calendar.businessDays(req.to, req.from),
       totalDays: moment(req.to).diff(req.from, 'days'),
       fromDate: req.from,
       toDate: req.to
     }
     return data
   },
-  businessDays: (toDate, fromDate) => {
-    let totalDays = moment(fromDate).diff(toDate, 'days')
-    //return totalDays - manager_calendar.amountWeekends(fromDate, toDate) - manager_calendar.amountHolidays(fromDate, toDate)
+  businessDays: (fromDate, toDate) => {
+    let holiday = 1
+    let initialDate = moment(fromDate, 'YYYY/MM/DD').format('MM/DD/YYYY')
+    let finishDate = moment(toDate, 'YYYY/MM/DD').format('MM/DD/YYYY')
+    let amountWeekends = manager_calendar.amountWeekends(`${finishDate}`, `${initialDate}`)
+
+    return moment(fromDate).diff(toDate, 'days') - (amountWeekends + holiday)
+  },
+  amountWeekends: (fromDate,toDate ) => {
+    var d0 = new Date(fromDate)
+    var d1 = new Date(toDate)
+
+    var ndays = 1 + Math.round((d1.getTime()-d0.getTime())/(24*3600*1000))
+    var nsaturdays = Math.floor((d0.getDay() + ndays) / 7)
+    return 2 * nsaturdays + (d0.getDay()==0) - (d1.getDay()==6)
   }
 }
 
